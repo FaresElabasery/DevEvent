26517868d490d9995b54037de4af747a8912154d
fares elabasery <81824063+FaresElabasery@users.noreply.github.com>
2025-11-15 04:05:36 +0200

    install posthog for track users

---
+++ instrumentation-client.ts
@@ -0,0 +1,10 @@
+import posthog from "posthog-js";
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  capture_pageview: true,
+  ui_host: "https://app.posthog.com",
+  defaults:'2025-05-24',
+  capture_exceptions: true,
+  debug:process.env.NODE_ENV === "development",
+});
